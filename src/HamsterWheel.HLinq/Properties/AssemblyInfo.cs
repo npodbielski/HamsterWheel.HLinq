015a9b774f693fb0e6e5818b294f9ba3639eca9b
natan <natan@podbielski.it>
2025-11-14 10:05:45 +0100

    feat(*): make internals visible to Flows

--- src/HamsterWheel.HLinq/Properties/AssemblyInfo.cs
+++ src/HamsterWheel.HLinq/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("HamsterWheel.HLinq.AspNet")]
 [assembly: InternalsVisibleTo("HamsterWheel.HLinq.Client")]
+[assembly: InternalsVisibleTo("HamsterWheel.Flows")]
