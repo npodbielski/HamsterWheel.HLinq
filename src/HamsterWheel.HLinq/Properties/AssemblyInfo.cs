c0425d27366542dc218fc9fa9eedec8b086fca69
natan <natan@podbielski.it>
2025-11-17 06:16:22 +0100

    feat(*): make internals visible to Data.Mapper

--- src/HamsterWheel.HLinq/Properties/AssemblyInfo.cs
+++ src/HamsterWheel.HLinq/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("HamsterWheel.HLinq.AspNet")]
 [assembly: InternalsVisibleTo("HamsterWheel.HLinq.Client")]
 [assembly: InternalsVisibleTo("HamsterWheel.Flows")]
+[assembly: InternalsVisibleTo("HamsterWheel.Data.Mapper")]
